f53b19c40297e695c38811f371ab6670187f51b1
faisalhanan <faisalhanan26@gmail.com>
2023-05-05 09:06:03 +0700

    tugas

---
+++ problem3.js
@@ -0,0 +1,7 @@
+// input
+let nama = "Faisal";
+let hello = "Hello";
+let pesan = "saya Javascript bahasa yang sangat menyenangkan"
+
+console.log(hello + " " + nama + "! " + pesan)
+// Your code here
---
+++ problem4.js
@@ -0,0 +1,5 @@
+//----MENGHITUNG LUAS SEGITIGA
+let alas = 20
+let tinggi = 30
+luas = 0.5*alas*tinggi
+console.log (luas);
